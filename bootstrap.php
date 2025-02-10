52ec7aa0198adb9be7eb3be0decfa49a977b75f9
Andreas van Loock <a.vanloock@toolpixx.com>
2025-02-10 23:06:28 +0100

    Add as new

--- bootstrap.php
+++ bootstrap.php
@@ -3,22 +3,22 @@
 defined('ABSPATH') || exit;
 
 /*
-Plugin Name: abilita PAY
-Plugin URI: https://abilita.de/
-Description: abilita PAY - coin4 RECHNUNG, coin4 LASTSCHRIFT, coin4 DIREKT, coin4 Vorkasse, PayPal, Kreditkarte
-Author: abilita GmbH
-Author URI: https://abilita.de/
-Tags: payment, gateway, ecommerce, e-commerce, store, sales, sell, shop, checkout, paypal, woo commerce, woocommmerce, invoice, sepa, advance, b2b, b2c
-Text Domain: abilita-payments-for-woocommerce
-Domain Path: /i18n/languages
+Plugin Name:          abilita PAY
+Plugin URI:           https://abilita.de/payment/zahlungsarten/
+Description:          abilita PAY - coin4 RECHNUNG, coin4 LASTSCHRIFT, coin4 DIREKT, coin4 Vorkasse, PayPal, Kreditkarte
+Author:               abilita GmbH
+Author URI:           https://abilita.de/
+Tags:                 payment, gateway, ecommerce, e-commerce, store, sales, sell, shop, checkout, paypal, woo commerce, woocommmerce, invoice, sepa, advance, b2b, b2c
+Text Domain:          abilita-payments-for-woocommerce
+Domain Path:          /i18n/languages
 
-Version: 1.0.0
-Requires at least: 6.0
-Requires PHP: 8.0.0
+Version:              1.0.0
+Requires at least:    6.0
+Requires PHP:         8.0.0
 
 WC requires at least: 8.0
-WC tested up to: 8.0
-Stable tag: 1.0.0
+WC tested up to:      8.0
+Stable tag:           1.0.0
 
 @copyright Copyright (c) 2025, abilita GmbH
 License: GNU General Public License v3.0
